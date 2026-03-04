f2daac8090371a831e21c5ad0a590dd93cea9dd0
timileyinjayeoba <timileyinjayeoba@gmail.com>
2026-03-04 11:24:58 +0100

    Initial commit - Retail ETL Pipeline

--- retail-etl-pipeline/src/extract.py
+++ retail-etl-pipeline/src/extract.py
@@ -1,6 +1,15 @@
 import pandas as pd
+from pathlib import Path
+
 
 def extract_data():
-    file_path = "data/raw_sales.csv"
+    # Get project root directory (retail-etl-pipeline/)
+    base_dir = Path(__file__).resolve().parent.parent
+    
+    # Build full path to CSV
+    file_path = base_dir / "data" / "raw_sales.csv"
+    
+    # Read file
     df = pd.read_csv(file_path)
-    return df
+    
+    # return df
